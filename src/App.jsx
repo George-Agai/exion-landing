f157da0b650844e135b882aff74b1405ac2fddc7
George Agai <georgeagai2000@gmail.com>
2025-08-18 16:26:40 +0300

    Delete account page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import LandingPage from './pages/LandingPage.jsx'
 import PrivacyPolicy from './pages/PrivacyPolicy.jsx'
 import TermsOfUse from './pages/TermsOfUse.jsx'
+import Delete from './pages/Delete.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 
@@ -12,6 +13,7 @@ function App() {
           <Route path='/' element={<LandingPage />} />
           <Route path='/privacy-policy' element={<PrivacyPolicy />} />
           <Route path='/terms-of-use' element={<TermsOfUse />} />
+          <Route path='/delete' element={<Delete />} />
         </Routes>
       </BrowserRouter>
     </div>
